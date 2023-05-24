313ae2a4a902ab804f2246a4500aeb813971e70d
stcastell <santicastell98@hotmail.com>
2023-05-24 15:33:42 -0400

    Right to center function added

--- RevealElementsOnScroll.js
+++ RevealElementsOnScroll.js
@@ -25,5 +25,27 @@ let RevealElementsOnScroll = {
             }
         });
     },
+    RightToCenter: (className, referenceClass) => {
+        const reference = document.getElementsByClassName(referenceClass)[0]
+        reference.addEventListener("scroll", function () {
+            var screen = window.innerHeight;
+            var hidden = document.getElementsByClassName(className);
+            for (var i = 0; i < hidden.length; i++) {
+                hidden[i].style.transition = '1000ms ease-out'
+                hidden[i].style.position = 'absolute'
+                hidden[i].style.transitionProperty = "right, opacity, transform"
+                var distance = hidden[i].getBoundingClientRect().top;
+                if (distance < screen) {
+                    hidden[i].style.right = "50%"
+                    hidden[i].style.transform = "translateX(50%)"
+                    hidden[i].style.opacity = 1
+                } else {
+                    hidden[i].style.right = "0%"
+                    hidden[i].style.transform = "translateX(0%)"
+                    hidden[i].style.opacity = 0
+                }
+            }
+        });
+    },
 }
 export default RevealElementsOnScroll
