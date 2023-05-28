9d07e9c8d9243ef5c6a5ee1811cfae4880f30a6a
stcastell <67725050+stcastell@users.noreply.github.com>
2023-05-28 14:35:22 -0400

    Right to left function added

--- RevealElementsOnScroll.js
+++ RevealElementsOnScroll.js
@@ -47,5 +47,27 @@ let RevealElementsOnScroll = {
             }
         });
     },
+    RightToLeft: (className, referenceClass) => {
+        const reference = document.getElementsByClassName(referenceClass)[0]
+        reference.addEventListener("scroll", function () {
+            var screen = window.innerHeight;
+            var hidden = document.getElementsByClassName(className);
+            for (var i = 0; i < hidden.length; i++) {
+                hidden[i].style.transition = '1000ms ease-out'
+                hidden[i].style.position = 'absolute'
+                hidden[i].style.transitionProperty = "right, opacity, transform"
+                var distance = hidden[i].getBoundingClientRect().top;
+                if (distance < screen) {
+                    hidden[i].style.right = "100%"
+                    hidden[i].style.transform = "translateX(100%)"
+                    hidden[i].style.opacity = 1
+                } else {
+                    hidden[i].style.right = "0%"
+                    hidden[i].style.transform = "translateX(0%)"
+                    hidden[i].style.opacity = 0
+                }
+            }
+        });
+    },
 }
 export default RevealElementsOnScroll
